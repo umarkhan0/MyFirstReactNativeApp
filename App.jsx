18518e239f50b8bde3205795ee1e837b6b70c424
Umar khan <umaraamir959@gmail.com>
2024-01-29 23:54:11 +0500

    ConfigureApp

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { View , Text } from "react-native";
+
+export default function App() {
+  return(
+    <View style={{flex: 1 , backgroundColor: "#000" ,  justifyContent: "center" , alignItems: "center"}}>
+      <Text  style={{
+        color: "#ffff"
+      }}>HI MY Name is Umar</Text>
+      <Text style={{
+        color: "#ffff"
+      }}>This is my First App</Text>
+
+    </View>
+  )
+}
